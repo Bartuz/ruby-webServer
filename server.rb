8e75e7e9485b0f161051d7e38ec005f61eac4b3e
Ash Darji <ashishdarji88@gmail.com>
2014-06-08 21:54:30 -0400

    refactored

--- server.rb
+++ server.rb
@@ -1,15 +1,31 @@
 # Require socket from Ruby Standard Library (stdlib)
 require 'socket'                                    
 
-# Socket to listen to defined host and port
-server = TCPServer.open(host, port)
+class Server
 
-# Output to stdout that server started                 
-puts "Server started on #{host}:#{port} ..."        
+def initialize
+	# Socket to listen to defined host and port
+	server = TCPServer.open(host,port)
+	puts "Accepting connections at #{host}:#{port}"
+end
+
+def accept_connection
+	# Wait for a client to connect. Accept returns a TCPSocket
+	client = server.accept
+end
+
+def close_connection
+	#Disconnect from the client
+	@client.close
+	puts "Disconnected at #{Time.now.ctime}."
+end
+
+def 
+
 
 #server runs forever
 loop do  
-	# Wait for a client to connect. Accept returns a TCPSocket
+	
 	client = server.accept
 
 	lines = []
@@ -24,8 +40,10 @@ loop do
 
  	#Output current time to client
 	client.puts(Time.now.ctime)
-
-	# Disconnect from the client
-  	client.close                                      
+                                    
 
   end
+
+
+
+
